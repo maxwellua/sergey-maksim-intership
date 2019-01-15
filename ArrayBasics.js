aca303864d7a833fda4bdbb6a950dc651fbac6eb
Sergey Borodin <justtrueserj@gmail.com>
2019-01-15 22:38:28 +0300

    The fourth task. About maximum and minimum elements in 2 arrays.

--- ArrayBasics.js
+++ ArrayBasics.js
@@ -66,28 +66,28 @@ f(arrThird);
 ///// 4 /////
 let arrFourthOne = [1,144646,144727,144629,144743,144730,];
 let arrFourthTwo = [144727,144728,144625,144647,];
-let arrFourthFinalyMin = [];
-let arrFourthFinalyMax = [];
-let arrFourthGrouped = [];
-arrFourthGrouped[0] = [];
-arrFourthGrouped[0][0] = arrFourthOne.map((value) => {return value});
-arrFourthGrouped[0][1] = arrFourthTwo.map((value) => {return value});
-
-for (let iteration = 0; iteration < 2; iteration++) {
-    arrFourthFinalyMax[iteration] = arrFourthGrouped[0][iteration].reduce( (maximum , current) => {
+let arrFourthMin = [];
+let arrFourthMax = [];
+    arrFourthMax[0] = arrFourthOne.reduce( (maximum , current) => {
         if (maximum < current) { maximum = current }
         return maximum;
-    } )
+    } );
+    arrFourthMax[1] = arrFourthTwo.reduce( (maximum , current) => {
+        if (maximum < current) { maximum = current }
+        return maximum;
+    } );
 
-}
-
-for (let iteration = 0; iteration < 2; iteration++) {
-    arrFourthFinalyMin[iteration] = arrFourthGrouped[0][iteration].reduce( (minimum , current) => {
+    arrFourthMin[0] = arrFourthOne.reduce( (minimum , current) => {
         if (current < minimum) { minimum = current }
         return minimum;
-    }, )
+    }, );
+    arrFourthMin[1] = arrFourthTwo.reduce( (minimum , current) => {
+        if (current < minimum) { minimum = current }
+        return minimum;
+    }, );
 
-}
-console.log(arrFourthFinalyMax);
-console.log(arrFourthFinalyMin);
+
+
+console.log(arrFourthMax);
+console.log(arrFourthMin);
 /////   /////
