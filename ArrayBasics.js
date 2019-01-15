a4d54f9954d82699e9816da9ca8a1d067d88df74
Sergey Borodin <justtrueserj@gmail.com>
2019-01-15 17:25:55 +0300

    1st task

---
+++ ArrayBasics.js
@@ -0,0 +1,25 @@
+const arr = [1,2,3,4,5,6,7,8,9,-40];
+
+///// 1 /////
+const averageOfArr = arr.reduce((average , value) => { return average += value });
+
+console.log( arr.filter((x) => { return x > averageOfArr }));
+
+/////   /////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
