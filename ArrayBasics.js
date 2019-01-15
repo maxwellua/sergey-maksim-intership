c04507528ad0fe360cf38465e867e95297191795
Sergey Borodin <justtrueserj@gmail.com>
2019-01-15 17:27:06 +0300

    2st task

--- ArrayBasics.js
+++ ArrayBasics.js
@@ -7,19 +7,17 @@ console.log( arr.filter((x) => { return x > averageOfArr }));
 
 /////   /////
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+///// 2 /////
+function getSameOfArr(currentArr) {
+    for (let iFirst = 0; iFirst < currentArr.length - 1; iFirst++){
+        for (let iSecond = iFirst + 1; iSecond < currentArr.length; iSecond++){
+            if ( currentArr[iFirst] === currentArr[iSecond] ){
+                return 'Есть!'
+            }
+        }
+    }
+    return 'Нет!'
+}
+console.log(getSameOfArr(arr));
+/////   /////
 
