822a8ebf81143c75430fcad15652a9966c9bc252
Sergey Borodin <justtrueserj@gmail.com>
2019-01-16 12:26:11 +0300

    Fixed variables and indents

--- Fib.js
+++ Fib.js
@@ -1,32 +1,30 @@
-
-let maximum = +prompt('Max');
-let firstNumber = +prompt('First');
-let secondNumber = +prompt('Second');
+const maximum = +prompt('Max');
+const firstNumber = +prompt('First');
+const secondNumber = +prompt('Second');
 let result = [ firstNumber , secondNumber ];
-let method = +prompt('Введите номер метода ');
-let counterForRecursion = maximum - 1 ;
+const method = +prompt('Введите номер метода ');
+const counterForRecursion = maximum - 1;
 
-switch ( method ) {
-    case 1 :
+switch (method) {
+    case 1:
         getFibGenerally();
         console.log(result);
         break;
 
-    case 2 :
+    case 2:
         getFibRecursion(counterForRecursion);
         console.log(result);
         break;
 }
-
-
-function getFibGenerally (){
-    for (let counter = 2 ; counter < maximum ; counter++ ){
-        result.push( result[counter-1] + result[counter-2] )
+function getFibGenerally() {
+    for (let counter = 2; counter < maximum; counter++ ){
+        result.push(result[counter-1] + result[counter-2])
     }
-    return result ;
+    return result;
 }
-
 function getFibRecursion(counterForRecursion) {
-    return result[counterForRecursion] = counterForRecursion <= 1 ? counterForRecursion : getFibRecursion(counterForRecursion - 1) + getFibRecursion(counterForRecursion - 2);
+    return result[counterForRecursion] = counterForRecursion <= 1
+        ? counterForRecursion
+        : getFibRecursion(counterForRecursion - 1) + getFibRecursion(counterForRecursion - 2);
 }
 
