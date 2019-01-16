ee344c2a58f38b58eba3a0ba69f1c54c055eaa32
Sergey Borodin <justtrueserj@gmail.com>
2019-01-16 16:20:58 +0300

    Deleted commas at arrays and changed types of variables.

--- 4.js
+++ 4.js
@@ -1,16 +1,16 @@
-let arrFourthOne = [1, 144646, 144727, 144629, 144743, 144730,];
-let arrFourthTwo = [144727, 144728, 144625, 144647,];
+const arrFourthOne = [1, 144646, 144727, 144629, 144743, 144730];
+const arrFourthTwo = [144727, 144728, 144625, 144647];
 let arrFourthMin = [];
 let arrFourthMax = [];
 
-arrFourthMax[0] = arrFourthOne.reduce( (maximum , current) => {
+arrFourthMax[0] = arrFourthOne.reduce((maximum , current) => {
     if (maximum < current) {
         maximum = current
     }
 
     return maximum;
 });
-arrFourthMax[1] = arrFourthTwo.reduce( (maximum , current) => {
+arrFourthMax[1] = arrFourthTwo.reduce((maximum , current) => {
     if (maximum < current) {
         maximum = current
     }
@@ -18,14 +18,14 @@ arrFourthMax[1] = arrFourthTwo.reduce( (maximum , current) => {
     return maximum;
 });
 
-arrFourthMin[0] = arrFourthOne.reduce( (minimum , current) => {
+arrFourthMin[0] = arrFourthOne.reduce((minimum , current) => {
     if (current < minimum) {
         minimum = current
     }
 
     return minimum;
 });
-arrFourthMin[1] = arrFourthTwo.reduce( (minimum , current) => {
+arrFourthMin[1] = arrFourthTwo.reduce((minimum , current) => {
     if (current < minimum) {
         minimum = current
     }
