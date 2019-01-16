df4435379a8c81eb104aebc4e8582bcb143f7498
Sergey Borodin <justtrueserj@gmail.com>
2019-01-16 11:45:42 +0300

    Fixed code style, made functions shorter.

--- 1.js
+++ 1.js
@@ -1,5 +1,5 @@
-const arr = [1,2,3,4,5,6,7,8,9,-40];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, -40];
+const averageOfArr = arr.reduce((average , value) => average += value );
+const arrFiltered = arr.filter((x) => x > averageOfArr);
 
-const averageOfArr = arr.reduce((average , value) => { return average += value });
-
-console.log( arr.filter((x) => { return x > averageOfArr }));
+console.log(arrFiltered);
