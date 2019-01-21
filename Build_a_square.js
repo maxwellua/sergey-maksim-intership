7901453310767d547f2e432056f47d7783427031
Sergey Borodin <justtrueserj@gmail.com>
2019-01-21 16:01:06 +0300

    Function getShape done.

---
+++ Build_a_square.js
@@ -0,0 +1,10 @@
+function getShape(value) {
+    let pluses = '';
+
+    for (let iteration = 0; iteration < value; iteration++) {
+        pluses += '+';
+        }
+    for (let iteration = 0; iteration < value; iteration++) {
+        console.log(pluses)
+    }
+}
