34f51f07f3155e2603ed4354b96fb6b12cf6784d
Sergey Borodin <justtrueserj@gmail.com>
2019-01-21 11:39:30 +0300

    All troubles fixed.

--- Divisors.js
+++ Divisors.js
@@ -1,10 +1,13 @@
 function divisors(argument) {
-    let arr = [];
-    for (let iteration = 2; iteration < argument; iteration++ ){
-        if (argument % iteration === 0 ) {
-            arr.push(iteration)}
+    const arrDivisors = [];
+    for (let iteration = 2; iteration < argument; iteration++) {
+        if (!(argument % iteration)) {
+            arrDivisors.push(iteration)
+        }
     }
-    if (arr.length === 0) {return `${argument} is prime`}
-    return arr;
+
+    return arrDivisors.length === 0
+        ? `${argument} is prime`
+        : arrDivisors;
 }
 console.log(divisors(12));
