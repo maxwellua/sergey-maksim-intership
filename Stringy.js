d71d81e0cf8d3474717343bbd7072cbb1ef82d22
Sergey Borodin <justtrueserj@gmail.com>
2019-01-21 11:59:13 +0300

    Fixed all mistakes.

--- Stringy.js
+++ Stringy.js
@@ -2,10 +2,11 @@ const string = 'Hello Masha';
 
 function getStringy(size) {
 let str = '';
-for (let counter = 1; counter < size+1; counter++) {
+for (let counter = 1; counter < size + 1; counter++) {
     str += counter % 2;
 }
 
 return str
 }
+
 console.log(getStringy(string.length));
