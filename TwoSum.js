10952b26be48a1af857674913c1801ca4439c36e
Sergey Borodin <justtrueserj@gmail.com>
2019-01-21 11:56:48 +0300

    Fixed all mistakes.

--- TwoSum.js
+++ TwoSum.js
@@ -1,14 +1,17 @@
-let arr = [1, 2, 3, 5, 10, 7];
-let index = 15;
-function getFinalyArr(currentArr, index) {
-    let finalyArr = [];
-    for (let stepOne = 0; stepOne < currentArr.length - 1; stepOne++) {
-        for (let stepTwo = stepOne + 1; stepTwo < currentArr.length; stepTwo++) {
-            if (currentArr[stepOne] + currentArr[stepTwo] === index) {
-                finalyArr.push(stepOne, stepTwo);
-                return finalyArr;
+const startArray = [1, 2, 3, 5, 10, 7];
+const index = 15;
+
+function getFinalyArr(currentArray, index) {
+    const arrayElements = [];
+    for (let stepOne = 0; stepOne < currentArray.length - 1; stepOne++) {
+        for (let stepTwo = stepOne + 1; stepTwo < currentArray.length; stepTwo++) {
+            if (currentArray[stepOne] + currentArray[stepTwo] === index) {
+                arrayElements.push(stepOne, stepTwo);
+
+                return arrayElements;
             }
         }
     }
 }
-console.log(getFinalyArr(arr, index));
+
+console.log(getFinalyArr(startArray, index));
