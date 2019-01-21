7acbda7bdabb73d47030dc0fa9c326a89cc8e04a
Sergey Borodin <justtrueserj@gmail.com>
2019-01-21 17:45:05 +0300

    Function for simple remove duplicates done.

---
+++ Simple_remove_duplicates.js
@@ -0,0 +1,19 @@
+function solve(solveArray, arrayDuplicates) {
+    let isTrue = false;
+
+        for (let iterationDuplicates = 0; iterationDuplicates < arrayDuplicates.length; iterationDuplicates++) {
+            for (let iterationBefore = solveArray.length; iterationBefore >= 0; iterationBefore--) {
+                if (isTrue && (arrayDuplicates[iterationDuplicates] == solveArray[iterationBefore])) {
+                    solveArray.splice(iterationBefore, 1)
+                }
+                if (arrayDuplicates[iterationDuplicates] == solveArray[iterationBefore]) {
+                    isTrue = true;
+                }
+            }
+            isTrue = false;
+        }
+
+        return solveArray
+}
+
+console.log(solve([3, 4, 4, 3, 6, 3], [4, 6, 3]));
