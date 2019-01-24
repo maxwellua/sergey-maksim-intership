291d1d65dbaececb3a6ba1c54ade5b2dc8bd601f
Sergey Borodin <justtrueserj@gmail.com>
2019-01-24 14:16:21 +0300

    Next troubles with node.js

--- MomentTry.js
+++ MomentTry.js
@@ -1,2 +1,4 @@
-let moment = require('moment');
-let time = moment().startOf('hour').fromNow();
+import moment from 'moment-es6';
+//import moment from './node_modules/moment/src/moment';
+
+console.log(moment(123456).fromNow());
