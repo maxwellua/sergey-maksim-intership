da2f338c332dff76292758fef2895af74b3bf23f
Sergey Borodin <justtrueserj@gmail.com>
2019-01-24 19:14:50 +0300

    Added new inheritance.

--- inheritance.js
+++ inheritance.js
@@ -44,3 +44,61 @@ meizux54.broken();
 meizux54.buttonOnCenter();
 meizux54.soundModule();
 console.log(meizux54);
+////////////////////////////////
+function Lada(name, created) {
+    this.name = name;
+    this.created = created;
+
+}
+Lada.prototype.isCar = function () {
+    this.created = '100 летней давности';
+    alert(this.name +' не является машиной!!!');
+};
+Lada.prototype.carUser =
+    this.carUser = 'Недалекий человек';
+
+function Priora(name) {
+    this.name = name;
+    Lada.apply(this, arguments);
+}
+
+Priora.prototype = Object.create(Lada.prototype);
+Lada.prototype.constructor = Priora;
+let lastochka = new Priora('Ласточка', 1879);
+console.log(lastochka);
+lastochka.isCar();
+console.log(lastochka.carUser);
+console.log(lastochka);
+/////////////////////////////////////////
+function Animal(name) {
+    this.name = name;
+    this.speed = 0;
+}
+
+Animal.prototype.stop = function() {
+    this.speed = 0;
+    alert(this.name + ' стоит');
+};
+
+Animal.prototype.run = function(speed) {
+    this.speed += speed;
+    alert(this.name + ' бежит, скорость ' + this.speed);
+};
+
+function Rabbit(name) {
+    this.name = name;
+    this.speed = 0;
+}
+
+Rabbit.prototype = Object.create(Animal.prototype);
+Rabbit.prototype.constructor = Rabbit;
+Rabbit.prototype.jump = function() {
+    this.speed++;
+    alert(this.name + ' прыгает, скорость ' + this.speed);
+};
+
+let krosh = new Rabbit('Кроля');
+console.log(krosh);
+krosh.stop();
+console.log(krosh);
+console.log(krosh);
