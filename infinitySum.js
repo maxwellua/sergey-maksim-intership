ad595dc3384cbddbfab0fbe11d80a4e19919f5f6
maxwell <ua.max.well@gmail.com>
2019-01-28 10:30:51 +0200

    CodeStyle improved

--- infinitySum.js
+++ infinitySum.js
@@ -1,25 +1,24 @@
-function sum(arg) { 
+function sum(arg) {
 	let totalSum = (!arg) ? 0 : arg;
-	
+
 	function addon(argg) {
-		totalSum += argg; 
+		totalSum += argg;
 		return addon;
 	};
-	
+
 	sum.toString = () => totalSum;
 	addon.toString = () => totalSum;
 	addon.reset = () => totalSum = 0;
-	
+
 	return addon;
 };
-  
-  let s = sum();
- 
-	console.log(s); // 0
-  s.reset();
-  console.log(s(1)); // 1
-  s.reset();
-  console.log(s(1)(2)); //3
-  s.reset();
-  console.log(s(3)(4)(5)); // 12
-  
+
+let s = sum();
+
+console.log(s); // 0
+s.reset();
+console.log(s(1)); // 1
+s.reset();
+console.log(s(1)(2)); //3
+s.reset();
+console.log(s(3)(4)(5)); // 12
